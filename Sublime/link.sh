8f5c4e0a02c67fdc4dd5cb547d9870c6fc69a0e5
Pavel Brylov <pavel@brylov.info>
2014-10-15 18:17:47 +0300

    Added sublime

---
+++ Sublime/link.sh
@@ -0,0 +1,2 @@
+ln -s ~/.yadr/Sublime/Package\ Control.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Package\ Control.sublime-settings
+ln -s ~/.yadr/Sublime/Preferences.sublime-settings      ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
